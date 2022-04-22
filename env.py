f6396c5be935d1110c907ea629cfd3d44f4c3ec8
cosmoostart <74989839+cosmoostart@users.noreply.github.com>
2022-04-23 00:38:37 +0500

    Create env.py

---
+++ env.py
@@ -0,0 +1,2 @@
+BOT_TOKEN= 5247968465:AAEhAIzt_ahDsSqhq1o5f4UQEMhjRDndJpw
+MONGO_URI= mongodb://laziz:UZWwSX7MIQmmPOnO@cluster0-shard-00-00.4tuag.mongodb.net:27017,cluster0-shard-00-01.4tuag.mongodb.net:27017,cluster0-shard-00-02.4tuag.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-bkrqrd-shard-0&authSource=admin&retryWrites=true&w=majority
