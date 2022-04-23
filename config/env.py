d628b13c71c712b3dc4f44233aef586d601856a3
cosmoostart <74989839+cosmoostart@users.noreply.github.com>
2022-04-23 07:39:00 +0500

    Update env.py

--- config/env.py
+++ config/env.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv('5247968465:AAEhAIzt_ahDsSqhq1o5f4UQEMhjRDndJpw')
-MONGO_URI = getenv('mongodb://laziz:UZWwSX7MIQmmPOnO@cluster0-shard-00-00.4tuag.mongodb.net:27017,cluster0-shard-00-01.4tuag.mongodb.net:27017,cluster0-shard-00-02.4tuag.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-bkrqrd-shard-0&authSource=admin&retryWrites=true&w=majority')
-SUDO_USERS = list(map(int, getenv('674193259').split()))
+BOT_TOKEN = getenv('BOT_TOKEN')
+MONGO_URI = getenv('MONGO_URI')
+SUDO_USERS = list(map(int, getenv('SUDO_USERS').split()))
